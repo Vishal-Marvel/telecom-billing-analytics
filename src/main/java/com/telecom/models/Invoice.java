d20f153af51af47f59ab64343390d8f1a5c5f309
Vishal-Marvel <vishal.pvn.edu@gmail.com>
2025-08-29 16:55:51 +0530

    Rectified Invoice.java

--- src/main/java/com/telecom/models/Invoice.java
+++ src/main/java/com/telecom/models/Invoice.java
@@ -1,9 +1,25 @@
 package com.telecom.models;
 
+import lombok.*;
+
+import java.time.YearMonth;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Invoice {
+    private String id;
+    private String subscriptionId;
+    private YearMonth billingCycle;
+
+    private double baseRental;
     private double overageCharges;
+    private double roamingCharges;
+    private double discounts;
+    private double taxes;
+    private double totalAmount;
+
+    private boolean paid;
+    private String notes;   // e.g., credit control, fairness surcharge info
 }
